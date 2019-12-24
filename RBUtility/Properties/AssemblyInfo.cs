291912f0ad68e05abf4d6714050285cb49d77822
Anil Nair <anil-rb-90@outlook.com>
2019-12-24 20:46:24 +0530

    Added function to read files, Updated function for creating database string.

--- RBUtility/Properties/AssemblyInfo.cs
+++ RBUtility/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RBUtility")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("File Utilities For Logging")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Bionic Hope Private Limited")]
 [assembly: AssemblyProduct("RBUtility")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Bionic Hope Private Limited Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- RBUtility/RBUtility.cs
+++ RBUtility/RBUtility.cs
@@ -1,12 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
+
 
 namespace RBUtility
 {
     public class RBUtility
     {
+        public static void WriteLog(string strMessage, string LogFileName = "RBUtility")
+        {
+            string Dt4 = DateTime.Now.AddDays(-2).ToString("dd/MM/yyyy").Substring(0, 2);
+            StringBuilder fileFormat = new StringBuilder();
+
+            fileFormat.Append(AppDomain.CurrentDomain.BaseDirectory);
+            fileFormat.Append("\\" + LogFileName + "Log");
+            fileFormat.Append(DateTime.Now.AddDays(-2).ToString("dd/MM/yyyy").Substring(0, 2));
+            fileFormat.Append(DateTime.Now.AddDays(-2).ToString("dd/MM/yyyy").Substring(3, 2));
+            fileFormat.Append(DateTime.Now.AddDays(-2).ToString("dd/MM/yyyy").Substring(6, 4));
+            fileFormat.Append(".log");
+
+            if (File.Exists(fileFormat.ToString()))
+            {
+                File.Delete(fileFormat.ToString());
+            }
+
+            fileFormat.Clear();
+
+            fileFormat.Append(AppDomain.CurrentDomain.BaseDirectory);
+            fileFormat.Append("\\" + LogFileName + "Log");
+            fileFormat.Append(DateTime.Now.Date.ToString("dd/MM/yyyy").Substring(0, 2));
+            fileFormat.Append(DateTime.Now.Date.ToString("dd/MM/yyyy").Substring(3, 2));
+            fileFormat.Append(DateTime.Now.Date.ToString("dd/MM/yyyy").Substring(6, 4));
+            fileFormat.Append(".log");
+
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(fileFormat.ToString(), true))
+            {
+                file.AutoFlush = false;
+                file.WriteLine(System.DateTime.Now.TimeOfDay.ToString().Substring(0, 5) + " " + strMessage);
+            }
+        }
+
+        public static string CreateDBconnectionString(string IniFileName)
+        {
+            string ConnectionString = string.Empty;
+
+            string C_ServerName = ReadLine(IniFileName);
+
+            string C_DbName = ReadLine(IniFileName);
+
+            string userName = ReadLine(IniFileName);
+
+            string password = ReadLine(IniFileName);
+
+            ConnectionString = "Data Source=" + C_ServerName + ";initial catalog=" + C_DbName + ";user Id=" + userName + ";password=" + password + "";
+
+            return ConnectionString;
+        }
+
+        public static string ReadLine(string file_name, char separator = '=')
+        {
+            using (StreamReader file = new StreamReader(file_name))
+            {
+                return file.ReadLine().Split(separator)[1].ToString();
+            }
+
+        }
     }
+
 }
+
